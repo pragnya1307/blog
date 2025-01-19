a2c9becdb579a3d24786d11f220b13bacc773f60
pragnya1307 <pragnyas2005@gmail.com>
2025-01-19 12:52:29 +0530

    Update local.session.sql

--- local.session.sql
+++ local.session.sql
@@ -7,12 +7,14 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 SELECT * FROM users;
-
 CREATE TABLE posts (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    post_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL, -- This references the user who created the post
+    username VARCHAR(200) NOT NULL,
     title VARCHAR(200) NOT NULL,
     content TEXT NOT NULL,
     image VARCHAR(200) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) -- Assuming a users table exists
 );
 SELECT * FROM posts;
